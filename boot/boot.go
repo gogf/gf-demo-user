da08ead679bff0b7a36f4501eebca959e4ba9db2
John <john@johng.cn>
2019-12-07 17:36:41 +0800

    gf version updates

--- boot/boot.go
+++ boot/boot.go
@@ -1,29 +1,6 @@
 package boot
 
-import (
-	"github.com/gogf/gf/frame/g"
-	"github.com/gogf/gf/net/ghttp"
-	"github.com/gogf/gf/os/glog"
-)
-
 // 用于应用初始化。
 func init() {
-	v := g.View()
-	c := g.Config()
-	s := g.Server()
 
-	// 模板引擎配置
-	v.AddPath("template")
-	v.SetDelimiters("${", "}")
-
-	// glog配置
-	logpath := c.GetString("setting.logpath")
-	glog.SetPath(logpath)
-	glog.SetStdoutPrint(true)
-
-	// Web Server配置
-	s.SetServerRoot("public")
-	s.SetLogPath(logpath)
-	s.SetNameToUriType(ghttp.URI_TYPE_ALLLOWER)
-	s.SetPort(8199)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/gogf/gf-demos
 
-require github.com/gogf/gf v1.9.10
+require github.com/gogf/gf v1.10.0

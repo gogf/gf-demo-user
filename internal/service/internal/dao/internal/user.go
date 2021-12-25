0df07e10244a577a7a8f04dfca0b674cb9927e4a
john <john@johng.cn>
2021-12-25 23:49:44 +0800

    dao update

--- internal/service/internal/dao/internal/user.go
+++ internal/service/internal/dao/internal/user.go
@@ -12,9 +12,9 @@ import (
 
 // UserDao is the data access object for table user.
 type UserDao struct {
-	Table   string      // Table is the underlying table name of the DAO.
-	Group   string      // Group is the database configuration group name of current DAO.
-	Columns UserColumns // Columns contains all the column names of Table for convenient usage.
+	table   string      // table is the underlying table name of the DAO.
+	group   string      // group is the database configuration group name of current DAO.
+	columns UserColumns // columns contains all the column names of Table for convenient usage.
 }
 
 // UserColumns defines and stores column names for table user.
@@ -40,20 +40,35 @@ var userColumns = UserColumns{
 // NewUserDao creates and returns a new DAO object for table data access.
 func NewUserDao() *UserDao {
 	return &UserDao{
-		Group:   "default",
-		Table:   "user",
-		Columns: userColumns,
+		group:   "default",
+		table:   "user",
+		columns: userColumns,
 	}
 }
 
 // DB retrieves and returns the underlying raw database management object of current DAO.
 func (dao *UserDao) DB() gdb.DB {
-	return g.DB(dao.Group)
+	return g.DB(dao.group)
+}
+
+// Table returns the table name of current dao.
+func (dao *UserDao) Table() string {
+	return dao.table
+}
+
+// Columns returns all column names of current dao.
+func (dao *UserDao) Columns() UserColumns {
+	return dao.columns
+}
+
+// Group returns the configuration group name of database of current dao.
+func (dao *UserDao) Group() string {
+	return dao.group
 }
 
 // Ctx creates and returns the Model for current DAO, It automatically sets the context for current operation.
 func (dao *UserDao) Ctx(ctx context.Context) *gdb.Model {
-	return dao.DB().Model(dao.Table).Safe().Ctx(ctx)
+	return dao.DB().Model(dao.table).Safe().Ctx(ctx)
 }
 
 // Transaction wraps the transaction logic using function f.

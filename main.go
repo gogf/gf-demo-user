b8bfd598983013040eb5e6e1fb5bc8b1202759e3
john <john@johng.cn>
2022-01-23 11:51:16 +0800

    up

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"github.com/gogf/gf-demos/v2/internal/cmd"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func main() {
-	var (
-		ctx = gctx.New()
-	)
-	if err := cmd.Main.Run(ctx); err != nil {
-		g.Log().Fatal(ctx, err)
-	}
+	cmd.Main.Run(gctx.New())
 }
